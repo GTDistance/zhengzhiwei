6303dbb55c92663fdbdf04085bf8b52428be5c6a
Thomas.Wang <zdhwanglc@163.com>
2020-01-07 15:04:37 +0800

    Initial commit

---
+++ src/pages/mine/index.tsx
@@ -0,0 +1,14 @@
+import Taro, {Component} from "@tarojs/taro";
+import {View} from "@tarojs/components";
+
+class Home extends Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    )
+  }
+}
+
+export default Home
